45976e82f77e18f3f0843a10cc62bfcba6d58552
yeonghoon.lee <djaakclsrn12@naver.com>
2023-02-04 00:10:40 +0900

    NEW: 로그인 페이지 추가

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # mobile-invitation-front
+chatGPT로 만드는 모바일 청첩장 프론트입니다.
 
+# Commit Rules
+```
+NEW: 신규 파일 및 기능 추가
+IMP: 기능 개선
+SCH: 설정 변경
+CLN: 코드 정리 및 리팩토링
+BUG: 버그 픽스
+TST: 테스트
+DOC: 문서작업
+RMV: 삭제
+```
 ## Project setup
 ```
 npm install
--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,85 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="login-container">
+    <div class="header">
+      <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="Google Logo" />
+    </div>
+    <form>
+      <div class="form-group">
+        <label for="username">Username</label>
+        <input v-model="username" type="text" id="username" />
+      </div>
+      <div class="form-group">
+        <label for="password">Password</label>
+        <input v-model="password" type="password" id="password" />
+      </div>
+      <div class="form-group">
+        <button @click="submit">Sign in</button>
+      </div>
+    </form>
+  </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.login-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 40px;
+}
+
+.header {
+  margin-bottom: 40px;
+}
+
+.form-group {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 20px;
+}
+
+label {
+  font-size: 14px;
+  margin-bottom: 10px;
+}
+
+input {
+  width: 300px;
+  height: 40px;
+  padding: 10px;
+  font-size: 16px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+button {
+  width: 300px;
+  height: 40px;
+  background-color: #4285f4;
+  color: #fff;
+  font-size: 16px;
+  border-radius: 4px;
+  border: none;
+  cursor: pointer;
 }
 </style>
+
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  methods: {
+    submit() {
+      // make a request to the server with the username and password
+      console.log(`Username: ${this.username} Password: ${this.password}`);
+    }
+  }
+};
+</script>
+
+
+
